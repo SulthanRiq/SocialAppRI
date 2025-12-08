d280aa09d27e5e04b0928748967ae023e4d450fd
MuhamadAhdanfauzan <m.ahdanfauzan123@gmail.com>
2025-12-09 00:54:15 +0700

    menambahkan controller Privacy setting

--- lib/features/Privacy_setting/controller/privacy_setting_controller.dart
+++ lib/features/Privacy_setting/controller/privacy_setting_controller.dart
@@ -5,7 +5,7 @@ class PrivacyController extends GetxController {
   final RxList<String> interests = <String>[
     'Technology',
     'Fitness',
-    'Food',
+    'Foods',
   ].obs;
 
   final RxBool isNudgeActive = true.obs;
